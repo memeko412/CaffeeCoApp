f15eefdb9f6cdcf5c5935291707aa039db30d7e2
memeko412 <110287795+memeko412@users.noreply.github.com>
2024-07-25 15:22:49 +1000

    Added Database Connections, Implemented Product model and view

---
+++ CaffeeCoApp/CaffeeCoApp/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using CaffeeCoApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaffeeCoApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(products => products.Id).ToList();
+            return View(products);
+        }
+    }
+}
---
+++ CaffeeCoApp/CaffeeCoApp/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CaffeeCoApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [MaxLength(100)]
+        public string Category { get; set; } = "";
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+        public string Description { get; set; } = "";
+        [MaxLength(100)]
+        public string ImageFileName { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
