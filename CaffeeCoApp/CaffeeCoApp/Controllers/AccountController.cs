3020bb4666c229ca95f48c9575662b1fe63bbd3a
memeko412 <110287795+memeko412@users.noreply.github.com>
2024-07-26 16:27:48 +1000

    Added register view

---
+++ CaffeeCoApp/CaffeeCoApp/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using CaffeeCoApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaffeeCoApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ CaffeeCoApp/CaffeeCoApp/Models/RegisterDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CaffeeCoApp.Models
+{
+    public class RegisterDto
+    {
+        [Required(ErrorMessage = "First Name is required"), MaxLength(100)]
+        public string FirstName { get; set; } = "";
+        [Required(ErrorMessage = "Last Name is required"), MaxLength(100)]
+        public string LastName { get; set; } = "";
+        [Required(ErrorMessage = "Email is required"), EmailAddress, MaxLength(100)]
+        public string Email { get; set; } = "";
+        [Phone(ErrorMessage = "Invalid phone number format")]
+        public string? PhoneNumber { get; set; }
+        [MaxLength(200)]
+        public string? Address { get; set; }
+        [Required,
+            RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{10,}$", ErrorMessage = "The Password must be at least 10 characters long, have capital and non-capital letters, and contain non-alphanumeric characters."),
+            MaxLength(20, ErrorMessage ="The password should not exceed 20 characters.")]
+        public string Password { get; set; } = "";
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = "";
+
+    }
+}
