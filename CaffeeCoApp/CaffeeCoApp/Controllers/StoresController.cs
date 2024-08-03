f212b42b9623b0abceb42870a45c0663117703af
memeko412 <110287795+memeko412@users.noreply.github.com>
2024-08-03 18:07:51 +1000

    Added Create New Store view, google maps functionalities

---
+++ CaffeeCoApp/CaffeeCoApp/Controllers/StoresController.cs
@@ -0,0 +1,29 @@
+﻿using CaffeeCoApp.Models;
+using CaffeeCoApp.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaffeeCoApp.Controllers
+{
+    [Authorize(Roles = "admin"), Route("/Admin/[controller]/{action=Index}/{id?}")]
+    public class StoresController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public StoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            IQueryable<Store> query = context.Stores;
+            var stores = query.ToList();
+            return View(stores);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
