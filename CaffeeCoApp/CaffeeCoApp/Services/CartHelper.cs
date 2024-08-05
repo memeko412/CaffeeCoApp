cb9d3c0a620b417fecec14d766b390219dc49405
memeko412 <110287795+memeko412@users.noreply.github.com>
2024-08-05 16:40:07 +1000

    Added saving and updating shopping cart

---
+++ CaffeeCoApp/CaffeeCoApp/Services/CartHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace CaffeeCoApp.Services
+{
+    public class CartHelper
+    {
+        public static Dictionary<int,int> GetCartDictionary(HttpRequest request, HttpResponse response)
+        {
+            string cookieValue = request.Cookies["shopping_cart"] ?? "";
+            try
+            {
+                var cart = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(cookieValue));
+                var dictionary = JsonSerializer.Deserialize<Dictionary<int, int>>(cart);
+                if (dictionary != null)
+                {
+                    return dictionary;
+                }
+            } catch (Exception)
+            {
+            }
+
+            if (cookieValue.Length > 0)
+            {
+                response.Cookies.Delete("shopping_cart");
+            }
+
+            return new Dictionary<int, int>();
+        }
+
+        public static int GetCartSize(HttpRequest request, HttpResponse response)
+        {
+            int cartSize = 0;
+            var cartDictionary = GetCartDictionary(request, response);
+            foreach (var pair in cartDictionary)
+            {
+                cartSize += pair.Value;
+            }
+            return cartSize;
+        }
+
+    }
+}
